7676258f66eda130bf8ec2e5b33315fdfb6f62a4
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2016-05-02 14:38:08 +0200

    Update

--- archive-brand.php
+++ archive-brand.php
@@ -23,15 +23,15 @@
 										<a href="<?php echo get_post_meta(get_the_ID(), 'website', true);?>" class="panel-body">
 											<div style="height: 268px; width: 100%; background: white url('<?php the_post_thumbnail_url('full');?>') no-repeat center center; background-size: contain;"></div>
 											<h4 class="product-title" style="padding-left: 15px;"><?php the_title();?> <small><?php the_terms(get_the_ID(), 'product-group');?></small></h4>
-											<div style="color: #2f383d !important; padding-left: 15px; background: white;">
-												<?php
-													$terms = get_the_terms(get_the_ID(), 'group');
-													$tags = array_map(function ($term) {
-														return $term->name;
-													}, $terms);
-													echo implode(', ', $tags);
-												?>
-											</div>
+<!--											<div style="color: #2f383d !important; padding-left: 15px; background: white; height: ">-->
+<!--												--><?php
+//													$terms = get_the_terms(get_the_ID(), 'group');
+//													$tags = array_map(function ($term) {
+//														return $term->name;
+//													}, $terms);
+//													echo implode(', ', $tags);
+//												?>
+<!--											</div>-->
 										</a>
 									</div>
 								</div>

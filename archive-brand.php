a22029ac5dd00b029a8a6d904a5458cbad1408ab
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2016-05-02 14:27:52 +0200

    Update

--- archive-brand.php
+++ archive-brand.php
@@ -21,7 +21,7 @@
 								<div class="col-md-4">
 									<div class="panel panel-default product grid">
 										<a href="<?php the_permalink();?>" class="panel-body">
-											<div style="height: 268px; width: 100%; background: url('<?php the_post_thumbnail_url('full');?>') no-repeat center center;"></div>
+											<div style="height: 268px; width: 100%; background: white url('<?php the_post_thumbnail_url('full');?>') no-repeat center center; background-size: contain;"></div>
 											<h4 class="product-title"><?php the_title();?> <small><?php the_terms(get_the_ID(), 'product-group');?></small></h4>
 										</a>
 										<a href="<?php the_permalink();?>" class="panel-footer">

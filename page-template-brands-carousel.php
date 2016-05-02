8b16a91c0d6b8cc1984439c882e978baae82a06d
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2016-05-02 12:13:48 +0200

    Update

--- page-template-brands-carousel.php
+++ page-template-brands-carousel.php
@@ -14,14 +14,16 @@
         ]);
 
         if($brands->have_posts()) : while($brands->have_posts()) : $brands->the_post();
+            global $dynamic_featured_image;
+            $featured_images = $dynamic_featured_image->get_featured_images( );
+            if($featured_images[0]['full']) :
     ?>
         <a class="brands-carousel-image" href="<?php the_permalink();?>">
             <?php
-                global $dynamic_featured_image;
-                $featured_images = $dynamic_featured_image->get_featured_images( );
+
             ?>
             <img src="<?php echo $featured_images[0]['full'];?>" style="max-height: 160px; float: left;">
         </a>
-    <?php endwhile; endif; wp_reset_postdata();?>
+    <?php endif; endwhile; endif; wp_reset_postdata();?>
 
 </div>
